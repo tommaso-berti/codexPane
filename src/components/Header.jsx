b48b0e76d4a9c26901b98f611dfe95046f09b2e0
Tommaso Berti <tommaso.berti@helptek.it>
2025-10-08 17:20:08 +0200

    implement routing with React Router and add layout components

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="content-center">
+            <h1 className="text-black">CodexPane</h1>
+        </header>
+    );
+}
---
+++ src/components/Layout.jsx
@@ -0,0 +1,11 @@
+import Header from './Header';
+import Footer from './Footer';
+import Sidebar from './Sidebar';
+
+export default function Layout() {
+    return (
+        <div>
+
+        </div>
+    );
+}
