c8044df1322ad9a51d0f233e532cc151802302b9
Tommaso Berti <tommaso.berti@helptek.it>
2025-10-15 17:39:22 +0200

    refactor layout and styling; adjust spacing and add box-sizing for improved layout consistency

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,13 +7,13 @@ export default function Layout() {
     return (
         <div className="flex flex-col min-h-screen w-full">
             <Header />
-            <div className="flex flex-row flex-1 mt-20 w-full h-[calc(100dvh-5rem)] min-h-0">
+            <div className="flex flex-row flex-1 mt-18 w-full h-[calc(100dvh-5rem)] min-h-0">
                 <div className="w-75  border-r fixed h-full">
                     <Sidebar />
                 </div>
                 <main
                     data-scroller="content"
-                    className="flex-1 pl-5 pt-3 min-h-0 overflow-auto ml-76"
+                    className="flex-1 pl-5 pr-5 pt-3 min-h-0 overflow-auto ml-76"
                 >
                     <Outlet />
                 </main>
--- src/components/SectMenuItem.jsx
+++ src/components/SectMenuItem.jsx
@@ -1,15 +1,12 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
 import ListItemText from "@mui/material/ListItemText";
 import Typography from "@mui/material/Typography";
 import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 import Collapse from "@mui/material/Collapse";
 import List from "@mui/material/List";
-import StarBorder from "@mui/icons-material/StarBorder";
 
 export default function SectMenuItem({section, subsections}) {
     const [open, setOpen] = useState(false);
@@ -27,9 +24,6 @@ export default function SectMenuItem({section, subsections}) {
     return (
         <>
             <ListItemButton key={section.id} onClick={handleClick}>
-                <ListItemIcon>
-                    <InboxIcon />
-                </ListItemIcon>
                 <ListItemText primary={<Typography noWrap>{section.title}</Typography>} />
                 {open ? <ExpandLess /> : <ExpandMore />}
             </ListItemButton>
@@ -37,9 +31,6 @@ export default function SectMenuItem({section, subsections}) {
                 <List component="div" disablePadding>
                     {subsections.map((subsection) => (
                         <ListItemButton key={subsection.id} onClick={() => handleSelect(subsection.slug)} sx={{ pl: 4 }}>
-                            <ListItemIcon>
-                                <StarBorder />
-                            </ListItemIcon>
                             <ListItemText primary={<Typography noWrap>{subsection.title}</Typography>} />
                         </ListItemButton>
                     ))}
