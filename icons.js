b0f22b5e998a463d1dccd8f3b4479c14983ea872
Tommaso Berti <tommaso.coding@gmail.com>
2025-10-27 10:34:17 +0100

    add icons for Terminal, CallMerge, GitHub, and update structure.json to include corresponding icons for HTML, CSS, Git, GitHub, and JavaScript

--- icons.js
+++ icons.js
@@ -2,12 +2,18 @@ import HtmlIcon from '@mui/icons-material/Html';
 import CssIcon from '@mui/icons-material/Css';
 import JavascriptIcon from '@mui/icons-material/Javascript';
 import SettingsIcon from '@mui/icons-material/Settings';
+import TerminalIcon from '@mui/icons-material/Terminal';
+import CallMergeIcon from '@mui/icons-material/CallMerge';
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 const ICONS = {
     HtmlIcon,
     CssIcon,
     JavascriptIcon,
-    SettingsIcon
+    SettingsIcon,
+    TerminalIcon,
+    CallMergeIcon,
+    GitHubIcon,
 };
 
 export default ICONS;
